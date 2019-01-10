b5523521d1101fdc2e720ef28d4a984f53473054
mpressen <mpressen@student.42.fr>
2019-01-10 08:08:11 +0100

    Init repo

---
+++ lib/mpressen_palindrome.rb
@@ -0,0 +1,6 @@
+require "mpressen_palindrome/version"
+
+module MpressenPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mpressen_palindrome_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MpressenPalindrome do
+  it "has a version number" do
+    expect(MpressenPalindrome::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
