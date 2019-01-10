abeba86f6a659965fa06d9ef171eb9e32c31b58e
mpressen <mpressen@student.42.fr>
2019-01-10 09:00:40 +0100

    Finish working on punctuation Palindrome. Tests suite green

--- lib/mpressen_palindrome.rb
+++ lib/mpressen_palindrome.rb
@@ -1,6 +1,22 @@
 require "mpressen_palindrome/version"
 
-module MpressenPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# module MpressenPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+
+# end
+
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
+
 end
--- spec/mpressen_palindrome_spec.rb
+++ spec/mpressen_palindrome_spec.rb
@@ -1,9 +1,25 @@
 RSpec.describe MpressenPalindrome do
+
   it "has a version number" do
     expect(MpressenPalindrome::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "test non-palindrome" do
+    expect("apple".palindrome?).not_to eq true
   end
+
+  it "test literal palindrome" do
+    expect("radar".palindrome?).to eq true
+  end
+
+  it "test mixed case palindrome" do
+    expect("RaceCar".palindrome?).to eq true
+  end
+
+  it "test palindrome with punctuation" do
+    expect("Madam, I’m Adam.".palindrome?).to eq true
+    expect("A man, a plan, a canal—Panama!".palindrome?).to eq true
+  end
+	
+
 end
