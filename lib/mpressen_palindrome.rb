f0e026be7dfddc4385e05a61b1c6c918e43ae23b
mpressen <mpressen@student.42.fr>
2019-02-13 09:44:12 +0100

    empty or whitespaces string are not palindrome

--- lib/mpressen_palindrome.rb
+++ lib/mpressen_palindrome.rb
@@ -1,20 +1,21 @@
-require "mpressen_palindrome/version"
+require 'mpressen_palindrome/version'
 
 module MpressenPalindrome
   class Error < StandardError; end
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
+    return false if processed_content.nil? || processed_content.empty?
+
     processed_content == processed_content.reverse
   end
 
   private
 
-    # Returns content for palindrome testing.
-    def processed_content
-      self.to_s.scan(/[a-z\d]/i).join.downcase
-    end
-
+  # Returns content for palindrome testing.
+  def processed_content
+    to_s.scan(/[a-z\d]/i).join.downcase.strip
+  end
 end
 
 class String
