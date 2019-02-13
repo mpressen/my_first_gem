cdf08942bfbdf3e16e1da7984efebe177e21f33c
mpressen <mpressen@student.42.fr>
2019-02-13 09:44:12 +0100

    empty or whitespaces string are not palindrome

--- spec/mpressen_palindrome_spec.rb
+++ spec/mpressen_palindrome_spec.rb
@@ -1,33 +1,38 @@
 RSpec.describe MpressenPalindrome do
-
-  it "has a version number" do
+  it 'has a version number' do
     expect(MpressenPalindrome::VERSION).not_to be nil
   end
 
-  it "test non-palindrome" do
-    expect("apple".palindrome?).to eq false
+  it 'test non-palindrome' do
+    expect('apple'.palindrome?).to eq false
   end
 
-  it "test literal palindrome" do
-    expect("radar".palindrome?).to eq true
+  it 'test literal palindrome' do
+    expect('radar'.palindrome?).to eq true
   end
 
-  it "test mixed case palindrome" do
-    expect("RaceCar".palindrome?).to eq true
+  it 'test empty string' do
+    expect(''.palindrome?).to eq false
   end
 
-  it "test palindrome with punctuation" do
-    expect("Madam, I’m Adam.".palindrome?).to eq true
-    expect("A man, a plan, a canal—Panama!".palindrome?).to eq true
-  end
-	
-  it "test non-palindrome with integers" do
-    expect(12345.palindrome?).to eq false
-
+  it 'test whitespaces filled string' do
+    expect("    \t".palindrome?).to eq false
   end
 
-  it "test palindrome with integers" do
-    expect(12321.palindrome?).to eq true
+  it 'test mixed case palindrome' do
+    expect('RaceCar'.palindrome?).to eq true
   end
 
+  it 'test palindrome with punctuation' do
+    expect('Madam, I’m Adam.'.palindrome?).to eq true
+    expect('A man, a plan, a canal—Panama!'.palindrome?).to eq true
+  end
+
+  it 'test non-palindrome with integers' do
+    expect(12_345.palindrome?).to eq false
+  end
+
+  it 'test palindrome with integers' do
+    expect(12_321.palindrome?).to eq true
+  end
 end
